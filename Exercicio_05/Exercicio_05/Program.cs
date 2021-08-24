bd34d346c425c38f6bd1078b91fa97ffd8e9f749
Israel <rookie.qodeless@outlook.com>
2021-08-23 23:45:52 -0300

    Realizado desafio 5: sistema de conexão com banco de dados

---
+++ Exercicio_05/Exercicio_05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Exercicio_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var bdConn = new MySqlConnection("server=localhost;database=exercicio_05;uid=root;pwd='Israel@46743562'");
+                bdConn.Open();
+                Console.WriteLine("Conectou!");
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error);
+                throw;
+            }
+        }
+    }
+}
